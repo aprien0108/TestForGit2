80162b19c3eba92705f73305fefe89cd10412624
Hyunwoo <aprien0108@naver.com>
2020-04-20 19:14:54 +0900

    kt 클래스 하나 만들어봄.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("하이");
-        println("좀 이상하넹");
 
 
     }
