afde783fc0b8b40d0ee2aae9e523d8b550ae8e8f
Hyunwoo <aprien0108@naver.com>
2020-04-26 14:46:43 +0900

    메인엑티비티 기능구성

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,17 +9,25 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val playroom : PlayroomActivity = PlayroomActivity()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mainPlayBtn = findViewById<Button>(R.id.main_btn_gamestart)
-        mainPlayBtn.setOnClickListener {
-            val toast = Toast.makeText(applicationContext, "ㅎㅇ", Toast.LENGTH_SHORT)
-            toast.show()
+        val playBtn = findViewById<Button>(R.id.main_btn_gamestart)
+        playBtn.setOnClickListener {
+//            val toast = Toast.makeText(applicationContext, "ㅎㅇ", Toast.LENGTH_SHORT)
+//            toast.show()
             val intent = Intent(this, PlayroomActivity::class.java).apply{
                 putExtra("하이","ㅎㅇㅎㅇ")
             }
+            startActivity(intent)
+        }
+        val setBtn = findViewById<Button>(R.id.main_btn_setting)
+        setBtn.setOnClickListener{
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+        val rankBtn = findViewById<Button>(R.id.main_btn_rank)
+        rankBtn.setOnClickListener{
+            val intent = Intent(this, RankActivity::class.java)
             startActivity(intent)
         }
 
