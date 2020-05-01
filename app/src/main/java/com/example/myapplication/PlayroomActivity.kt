b16c5b6760059299943013bef45ab03ef0083c1b
Hyunwoo <aprien0108@naver.com>
2020-05-01 14:51:01 +0900

    drawBlock을 사용해보았는데, 하나의 img view를 가지고 그리는거라, 호출할 때 마다 기존에 그려졌던 것들이 사라지게됨. 요걸 싹 다 저장해두는 방식을 추가할 계획임.

--- app/src/main/java/com/example/myapplication/PlayroomActivity.kt
+++ app/src/main/java/com/example/myapplication/PlayroomActivity.kt
@@ -1,7 +1,15 @@
 package com.example.myapplication
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class PlayroomActivity : AppCompatActivity() {
@@ -11,8 +19,10 @@ class PlayroomActivity : AppCompatActivity() {
         setContentView(R.layout.activity_playroom)
         //여기서 특정 조건마다 발동되는, drawmap 메소드가 필요행.
         //drawmap(map) 요거는, map에서 각 좌표에 해당하는 모든 block들을 가져와서 layout의 view와 매칭시켜주는 함수야.
-        gameStart()
-
+//        gameStart()
+        drawBlock(0,0)
+        drawBlock(1,1)
+        drawBlock(2,2)
 
     }
 
@@ -31,13 +41,41 @@ class PlayroomActivity : AppCompatActivity() {
         //맵 객체를 생성하고, 맵 객체는 생성시 각 좌표에 대한 블럭들을, 빈 상태로 만듦
         //맵 객체에는, 한줄이 완성되었거나 동일색상이 여러개 중첩되었는지를 판단하여 부수는 메소드가 존재해야함.
         val map = Map(20, 10)
-        var mapimgview = ImageView(this)
-        mapimgview.layoutParams.height = 32
-        mapimgview.layoutParams.width = 32
+//        var mapimgview = ImageView(this)
+//        mapimgview.layoutParams.height = 32
+//        mapimgview.layoutParams.width = 32
 //        mapimgview.id =
     }
 
     fun drawmap(map:Map) {
+
+    }
+
+    fun drawBlock(row:Int, column:Int) {//블럭 하나를 해당 좌표에 그리는 것.
+        val primg = findViewById<ImageView>(R.id.pr_map)
+        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
+        val canvas: Canvas = Canvas(bitmap)
+        var shapeDrawable: ShapeDrawable
+        val left = 0 + column * 50
+        val top = 0 + row * 38
+        val right = 50 + column * 50
+        val bottom = 38 + row * 38
+        val outr = floatArrayOf(12f,12f,12f,12f,12f,12f,12f,12f)//느낌알았다 근데 이걸 글로어케쓰지
+        //          2   3
+        //      1           4
+        //      8           6
+        //          7   5
+        //의 순서임. 각각 해당 부분의 원의 반지름을 정하는 것.
+        //
+        //
+        val inset = RectF(6F, 6F, 6F, 6F)//이게 테두리의 두께임.
+        val innerr = floatArrayOf(12f,12f,12f,12f,12f,12f,12f,12f)
+        shapeDrawable = ShapeDrawable(RoundRectShape(outr,inset,innerr))
+        shapeDrawable.setBounds( left, top, right, bottom)
+        shapeDrawable.getPaint().setColor(Color.parseColor("#009944"))
+        shapeDrawable.draw(canvas)
+        primg.background = BitmapDrawable(getResources(), bitmap)
+
 
     }
 
